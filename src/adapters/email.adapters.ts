6997151ef575d765627e031ceaebde0e8207c31f
Tsimofei <zhalianhouski@gmail.com>
2023-04-13 14:38:10 +0300

    Commit hw-14

--- src/adapters/email.adapters.ts
+++ src/adapters/email.adapters.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { User } from 'src/features/users/applications/users.schema';
 import nodemailer from 'nodemailer';
-import { EmailSenderDTO } from './email.dto';
 
 const senderData = {
   service: 'gmail',
@@ -10,29 +9,55 @@ const senderData = {
     pass: 'zfzmivezoxwgectq',
   },
 };
-const transporter = nodemailer.createTransport(
-  new EmailSenderDTO('gmail', {
+const transporter = nodemailer.createTransport({
+  port: 465,
+  host: 'smtp.gmail.com',
+  auth: {
     user: senderData.auth.user,
     pass: senderData.auth.pass,
-  }),
-);
+  },
+  secure: true,
+});
 
 @Injectable()
 export class EmailAdapter {
-  sendEmail(user: User) {
-    transporter.sendMail({
-      from: 'SAMURAIS-API, <bonypiper@gmail.com>',
-      to: user.accountData.email,
-      subject: 'Registration',
-      html: `<h1>Thank for your registration</h1>
+  async sendEmail(user: User) {
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log('Server is ready to take our messages');
+          resolve(success);
+        }
+      });
+    });
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(
+        {
+          from: 'SAMURAIS-API, <bonypiper@gmail.com>',
+          to: user.accountData.email,
+          subject: 'Registration',
+          html: `<h1>Thank for your registration</h1>
             <p>To finish registration please follow the link below:
             <a href='https://hw-incubator-nest.vercel.app/registration-confirmation?code=${user.emailConfirmation.confirmationCode}'>complete registration</a>
             </p>`,
+        },
+        (err, info) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+          } else {
+            console.log(info);
+            resolve(info);
+          }
+        },
+      );
     });
-    return true;
   }
-  sendEmailForPasswordRecovery(user: User) {
-    transporter.sendMail({
+  async sendEmailForPasswordRecovery(user: User) {
+    await transporter.sendMail({
       from: 'SAMURAIS-API, <bonypiper@gmail.com>',
       to: user.accountData.email,
       subject: 'Password Recovery',
@@ -41,6 +66,5 @@ export class EmailAdapter {
                            <a href='https://incubator-hw.vercel.app/password-recovery?recoveryCode=${user.passwordRecovery?.code}'>recovery password</a>
                         </p>`,
     });
-    return true;
   }
 }

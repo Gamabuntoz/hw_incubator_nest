44e23d5f175e435a6d4bfa6384882af39a8c6340
Tsimofei <zhalianhouski@gmail.com>
2023-04-13 15:02:23 +0300

    Commit hw-14

--- src/adapters/email.adapters.ts
+++ src/adapters/email.adapters.ts
@@ -3,19 +3,18 @@ import { User } from 'src/features/users/applications/users.schema';
 import nodemailer from 'nodemailer';
 import { senderData } from '../features/auth/applications/constants';
 
-const transporter = nodemailer.createTransport({
-  port: 465,
-  host: 'smtp.gmail.com',
-  auth: {
-    user: senderData.user,
-    pass: senderData.pass,
-  },
-  secure: true,
-});
-
 @Injectable()
 export class EmailAdapter {
   async sendEmail(user: User) {
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: 'smtp.gmail.com',
+      auth: {
+        user: senderData.user,
+        pass: senderData.pass,
+      },
+      secure: true,
+    });
     await new Promise((resolve, reject) => {
       transporter.verify(function (error, success) {
         if (error) {
@@ -51,14 +50,47 @@ export class EmailAdapter {
     });
   }
   async sendEmailForPasswordRecovery(user: User) {
-    await transporter.sendMail({
-      from: 'SAMURAIS-API, <bonypiper@gmail.com>',
-      to: user.accountData.email,
-      subject: 'Password Recovery',
-      html: `<h1>Password recovery</h1>
+    const transporter = nodemailer.createTransport({
+      port: 465,
+      host: 'smtp.gmail.com',
+      auth: {
+        user: senderData.user,
+        pass: senderData.pass,
+      },
+      secure: true,
+    });
+    await new Promise((resolve, reject) => {
+      transporter.verify(function (error, success) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        } else {
+          console.log('Server is ready to take our messages');
+          resolve(success);
+        }
+      });
+    });
+    await new Promise((resolve, reject) => {
+      transporter.sendMail(
+        {
+          from: 'SAMURAIS-API, <bonypiper@gmail.com>',
+          to: user.accountData.email,
+          subject: 'Password Recovery',
+          html: `<h1>Password recovery</h1>
                         <p>To finish password recovery please follow the link below:
                            <a href='https://incubator-hw.vercel.app/password-recovery?recoveryCode=${user.passwordRecovery?.code}'>recovery password</a>
                         </p>`,
+        },
+        (err, info) => {
+          if (err) {
+            console.error(err);
+            reject(err);
+          } else {
+            console.log(info);
+            resolve(info);
+          }
+        },
+      );
     });
   }
 }

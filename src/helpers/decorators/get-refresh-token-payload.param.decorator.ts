fb043181f2542bc164877703233592352a4bff77
Tsimofei <zhalianhouski@gmail.com>
2023-04-30 11:43:26 +0300

    Commit hw-15

--- src/helpers/decorators/get-refresh-token-payload.param.decorator.ts
+++ src/helpers/decorators/get-refresh-token-payload.param.decorator.ts
@@ -4,7 +4,7 @@ export const RefreshTokenPayload = createParamDecorator(
   (data: unknown, context: ExecutionContext) => {
     const request = context.switchToHttp().getRequest();
     return {
-      userId: request.user.userId,
+      userId: request.user.id,
       deviceId: request.user.deviceId,
       issueAt: request.user.issueAt,
     };
